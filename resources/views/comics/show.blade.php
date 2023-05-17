c92d1b564546897f1beb8c44c9c0b9f1ebcbe868
domenicobacoli <domenico.bacoli89@gmail.com>
2023-05-17 18:14:45 +0200

    feat: added Bonus

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -37,15 +37,35 @@
             </section>
             <div class="button-edit">
                 <a href="{{route('comics.edit', $comic->id)}}" class="me-3"><button>Modifica</button></a>
-
-                <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Elimina</button>
+                    <button type="submit" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Elimina
+                      </button>
                 </form>
-            </div>
-            
 
+                {{-- MODAL --}}
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">Attenzione!</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          Vuoi davvero eliminare definitivamente questo fumetto?
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" data-bs-dismiss="modal">Annulla</button>
+
+                            {{-- Form per l'eliminazione del comic dal database --}}
+                            <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">Elimina</button> 
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            </div>
         </div>
 
 
